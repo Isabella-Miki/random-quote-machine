4210c115a464839d6cdbefce690ec8699b32dbf5
Isabella Kobayachi <isabellamiki2004@gmail.com>
2023-07-10 08:29:08 -0300

    chaging fetch to axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 export default function App() {
   const [quote, setQuote] = useState("Pipoca é a resposta para tudo!");
 
-  const getQuote = () => {
-    fetch("https://type.fit/api/quotes")
-      .then((res) => res.json())
-      .then((data) => {
-        let randomNumData = Math.floor(Math.random() * data.length);
-        setQuote(data[randomNumData]);
-      });
+  const getQuote = async () => {
+    const response = await axios.get("https://type.fit/api/quotes");
+    let randomNumData = Math.floor(Math.random() * response.data.length);
+    setQuote(response.data[randomNumData]);
   };
 
   const colors = [
@@ -24,23 +22,23 @@ export default function App() {
     "#355834",
     "#111D4A",
     "#360A14",
-    "#6153CC"
+    "#6153CC",
   ];
 
   const [currentColor, setCurrentColor] = useState("#1E3F20");
 
   const getColor = () => {
     let randomNum = Math.floor(Math.random() * colors.length);
-    if(currentColor === colors[randomNum]) {
+    if (currentColor === colors[randomNum]) {
       randomNum = Math.floor(Math.random() * colors.length);
     } else {
       setCurrentColor(colors[randomNum]);
     }
   };
 
-  const getQuoteAndColor = () => {
+  const getColorAndQuote = async () => {
+    await getQuote();
     getColor();
-    getQuote();
   };
 
   const Wrapper = styled.div`
@@ -51,23 +49,27 @@ export default function App() {
   const Button = styled.button`
     background-color: ${currentColor};
     color: white;
-    padding: .5rem 1rem;
-    border-radius: .5rem;
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
     font-size: 1rem;
     transition: 0.4s;
 
-      &:hover {
-        transform: scale(1.1, 1.1);
-        transition: 0.4s;
-      }
+    &:hover {
+      transform: scale(1.1, 1.1);
+      transition: 0.4s;
+    }
   `;
 
   return (
     <Wrapper className={`grid content-center justify-center h-screen text-2xl`}>
       <div className="p-8 rounded bg-white max-w-md">
         <div>
-          <p className="text-justify">{quote.text === undefined ? quote : quote.text}</p>
-          <p className="mt-3 text-right text-xl">- {quote.author !== null ? quote.author : 'anonymous'}</p>
+          <p className="text-justify">
+            {quote.text === undefined ? quote : quote.text}
+          </p>
+          <p className="mt-3 text-right text-xl">
+            - {quote.author !== null ? quote.author : "anonymous"}
+          </p>
         </div>
         <div className="flex justify-between mt-5">
           <Button>
@@ -77,11 +79,7 @@ export default function App() {
               Tweet
             </a>
           </Button>
-          <Button
-            onClick={getQuoteAndColor}
-          >
-            New quote
-          </Button>
+          <Button onClick={getColorAndQuote}>New quote</Button>
         </div>
       </div>
     </Wrapper>
